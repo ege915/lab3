106d5b9c4ce626b53388916526d53e7644eab922
Elizabeth Ge <ege1915@gmail.com>
2023-04-19 16:47:47 -0700

    Create ListTests.java

---
+++ ListTests.java
@@ -0,0 +1,9 @@
+//import static org.junit.Assert.*;
+//import org.junit.*;
+
+//public class ArrayTests {
+//   @Test 
+//    public void testString(){
+//        assertEquals(0.0, ArrayExamples.averageWithoutLowest(input3), 0.01);
+//    }
+//}
