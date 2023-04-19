9d09544d9fd1f695e00c16bd01430ba5cb73293d
Elizabeth Ge <ege1915@gmail.com>
2023-04-19 16:47:45 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -16,6 +16,12 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
 
+  @Test
+  public void testReversed2(){
+    int[] input2 = {1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed2(input2));
+  }
+
  // @Test
   //public void testReverseInPlace2(){
   //  int[] input2 = {1, 2, 3};
@@ -29,15 +35,15 @@ public class ArrayTests {
     assertEquals(0.0, ArrayExamples.averageWithoutLowest(input3), 0.01);
   }
 
-  @Test 
-  public void testAverage2(){
-    double[] input3 = {1.0, 1.0};
-    assertEquals(1.0, ArrayExamples.averageWithoutLowest(input3), 0.01);
-  }
+//  @Test 
+//  public void testAverage2(){
+//    double[] input3 = {1.0, 1.0};
+//    assertEquals(1.0, ArrayExamples.averageWithoutLowest(input3), 0.01);
+//  }
 
-  @Test
-  public void testAverage3(){
-    double[] input4 = {0.0, 2.0, 4.0, -1.2, -1.2};
-    assertEquals(2.0, ArrayExamples.averageWithoutLowest(input4), 0.1);
-  }
+//  @Test
+//  public void testAverage3(){
+//    double[] input4 = {0.0, 2.0, 4.0, -1.2, -1.2};
+//    assertEquals(2.0, ArrayExamples.averageWithoutLowest(input4), 0.1);
+//  }
 }
